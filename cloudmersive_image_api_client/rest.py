5036b241035e9490cb21aba68afcfe6ce8975027
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2018-06-23 19:04:15 -0700

    Updated

--- cloudmersive_image_api_client/rest.py
+++ cloudmersive_image_api_client/rest.py
@@ -218,12 +218,8 @@ class RESTClientObject(object):
 
             # In the python 3, the response.data is bytes.
             # we need to decode it to string.
-
-            try:
-                if six.PY3:
-                    r.data = r.data.decode('utf8')
-            except:
-                logger.debug("binary response")
+            if six.PY3:
+                r.data = r.data.decode('utf8')
 
             # log response body
             logger.debug("response body: %s", r.data)
